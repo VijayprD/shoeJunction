dcd7590140dbac323ce2a5a5a48b4a55574c8a35
vijay pr dubey <vijaypd523@gmail.com>
2023-09-21 13:42:48 +0530

    mobile friendly navbar

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // responsive navbar 
 let responsive=document.getElementById('mob-res');
-let opacity=document.getElementById('hero');
 function responsiveNavbar(){
     responsive.classList.toggle('mob-mid')
-    opacity.classList.toggle('opacity')
 }
